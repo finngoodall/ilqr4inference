5d731937b21e5d902aef862f8f3ed51ad112bf97
finngoodall <finn.goodall@gmail.com>
2023-03-25 12:11:10 +0000

    Only compute covariances at the end

--- lib/lqr.py
+++ lib/lqr.py
@@ -56,49 +56,35 @@ class iLQR():
     def _rollout(
             self,
             us_init: List[NDArray]
-        ) -> Tuple[List[Gaussian]]:
-        """Compute the initial guess of the optimal state trajectory. Returns
-        the states and inputs as lists of `Gaussian` objects with placeholder
-        identity covariances.
+        ) -> NDArray:
+        """Compute the initial guess of the optimal state trajectory.
 
         Parameters
         - `us_init`:
             The initial guess for the optimal control inputs
 
         Returns
-        - `Tuple` of...
-            - `xs`:
-                The state trajectory
-            - `us`:
-                The control inputs with means equal to `us_init`
+        - `xs`:
+            The state trajectory
         """
 
-        # Placeholder covariances; these are never used in calculations
-        cov_x = np.eye(self.dynamics.Nx)
-        cov_u = np.eye(self.dynamics.Nu)
-
-        xs = []
-        us = []
+        xs = np.zeros((self.T, self.dynamics.Nx))
 
         if self.x0_prior:
-            x = self.x0_prior
+            x = self.x0_prior.mean
         else:
-            x = Gaussian(np.zeros(self.dynamics.Nx), cov_x)
+            x = np.zeros(self.dynamics.Nx)
 
-        for t, u_mean in enumerate(us_init):
-            u = Gaussian(u_mean, cov_u)
-            
-            xs.append(x)
-            us.append(u)
+        for t, u in enumerate(us_init):
+            xs[t] = x
+            x = self.dynamics.f(x, u, t)
 
-            x = Gaussian(self.dynamics.f(x.mean, u_mean, t), cov_x)
-
-        return xs, us
+        return xs
 
     def _cost(
             self,
-            xs: List[Gaussian],
-            us: List[Gaussian]
+            xs: NDArray,
+            us: NDArray
         ) -> float:
         """Calculate the cost of the passed trajectory.
         
@@ -115,12 +101,12 @@ class iLQR():
 
         cost = 0
         if self.x0_prior:
-            v = xs[0].mean - self.x0_prior.mean
+            v = xs[0] - self.x0_prior.mean
             cost += 0.5 * v.T @ pd_svd_inv(self.x0_prior.cov) @ v
 
         for t in range(self.T):
-            cost -= self.meas_model.ll(xs[t].mean, self.ys[t], t)
-            cost -= self.input_prior.ll(us[t].mean, t)
+            cost -= self.meas_model.ll(xs[t], self.ys[t], t)
+            cost -= self.input_prior.ll(us[t], t)
 
         return cost
 
@@ -200,21 +186,16 @@ class iLQR():
 
         return ks, Ks, v, Vs, Q_uu_invs
 
-    def _compute_trajectory(
+    def _compute_means(
             self,
             a: float,
-            xs_old: List[Gaussian],
-            us_old: List[Gaussian],
+            xs_old: NDArray,
+            us_old: NDArray,
             ks: NDArray,
             Ks: NDArray,
-            v1: NDArray,
-            Vs: NDArray,
-            Q_uu_invs: NDArray,
-            F_xs: NDArray,
-            F_us: NDArray,
-            C_xxs: NDArray,
-            C_uus: NDArray
-        ) -> Tuple[List[Gaussian]]:
+            v0: NDArray,
+            V0: NDArray
+        ) -> Tuple[NDArray]:
         """Compute the optimal state trajectory and control inputs.
         
         Parameters
@@ -228,8 +209,62 @@ class iLQR():
             The linear feedback vectors for the control inputs
         - `Ks`:
             The linear feedback gains for the control inputs
-        - `v1`:
+        - `v0`:
             The optimal cost-to-go vector at timestep 0
+        - `V0`:
+            The optimal cost-to-go Hessian at timestep 0
+        
+        Returns
+        - `xs`:
+            The updated optimal state trajectory
+        - `us`:
+            The updated optimal control inputs
+        """
+        
+        xs = np.zeros((self.T, self.dynamics.Nx))
+        us = np.zeros((self.T, self.dynamics.Nu))
+
+        for t in range(self.T):
+            if t == 0:
+                if self.x0_prior:
+                    P = pd_svd_inv(self.x0_prior.cov)
+                    x_cov = pd_svd_inv(P + V0)
+                    x = xs_old[0] + a*x_cov@(P@self.x0_prior.mean - v0)
+                else:
+                    x = xs_old[0] - a*pd_svd_inv(V0)@v0
+            else:
+                x = self.dynamics.f(x, u, t)
+
+            u = us_old[t] + Ks[t]@(x - xs_old[t]) + a*ks[t]
+
+            xs[t] = x
+            us[t] = u
+
+        return xs, us
+
+    def _compute_covs(
+            self,
+            xs: NDArray,
+            us: NDArray,
+            Ks: NDArray,
+            Vs: NDArray,
+            Q_uu_invs: NDArray,
+            F_xs: NDArray,
+            F_us: NDArray,
+            C_xxs: NDArray,
+            C_uus: NDArray
+        ) -> Tuple[List[Gaussian]]:
+        """Compute the covariances around the optimal state trajectory and
+        control inputs. Returns the Gaussian distributions over the states and
+        inputs.
+        
+        Parameters
+        - `xs`:
+            The optimal state trajectory means
+        - `us`:
+            The optimal control input means
+        - `Ks`:
+            The linear feedback gains for the control inputs
         - `Vs`:
             The optimal cost-to-go Hessians
         - `Q_uu_invs`:
@@ -239,48 +274,31 @@ class iLQR():
         - `F_us`:
             The dynamic's input Jacobians
         - `C_xxs`:
-            The cost function's state Hessians 
+            The cost function's state Hessians
         - `C_uus`:
             The cost function's input Hessians
         
         Returns
-        - `xs`:
-            The updated optimal state trajectory
-        - `us`:
-            The updated optimal control inputs
+        - Tuple of...
+            - `xs_with_covs`:
+                Gaussian distributions around the optimal state means
+            - `us_with_covs`:
+                Gaussian distributions around the optimal control input means
         """
-        
-        xs = []
-        us = []
+
+        xs_with_covs = []
+        us_with_covs = []
+
+        if self.x0_prior:
+            P = pd_svd_inv(self.x0_prior.cov)
+        else:
+            P = np.zeros((self.dynamics.Nx, self.dynamics.Nx))
 
         for t in range(self.T):
-            if t == 0:
-                if self.x0_prior:
-                    P = pd_svd_inv(self.x0_prior.cov)
-                    x_cov = pd_svd_inv(P + Vs[t])
-                    x = Gaussian(
-                        xs_old[0].mean + a*x_cov@(P@self.x0_prior.mean - v1),
-                        x_cov
-                    )
-                else:
-                    P = np.zeros((self.dynamics.Nx, self.dynamics.Nx))
-                    x = Gaussian(
-                        xs_old[0].mean - a*pd_svd_inv(Vs[t])@v1,
-                        pd_svd_inv(Vs[t])
-                    )
-            else:
-                x = Gaussian(
-                    self.dynamics.f(x.mean, u.mean, t),
-                    pd_svd_inv(P + Vs[t])
-                )
-
-            u = Gaussian(
-                us_old[t].mean + Ks[t]@(x.mean - xs_old[t].mean) + a*ks[t],
-                Ks[t]@x.cov@Ks[t].T + Q_uu_invs[t]
-            )
-
-            xs.append(x)
-            us.append(u)
+            x = Gaussian(xs[t], pd_svd_inv(P + Vs[t]))
+            u = Gaussian(us[t], Ks[t] @ x.cov @ Ks[t].T + Q_uu_invs[t])
+            xs_with_covs.append(x)
+            us_with_covs.append(u)
 
             F_x_inv = np.linalg.inv(F_xs[t])
             P1 = F_x_inv.T @ (P + C_xxs[t]) @ F_x_inv
@@ -288,22 +306,17 @@ class iLQR():
             P = P1 - P1@F_us[t]@P2@F_us[t].T@P1.T
             P = (P + P.T)/2
 
-        return xs, us
+        return xs_with_covs, us_with_covs
 
     def _forward_pass(
             self,
-            xs_old: List[Gaussian],
-            us_old: List[Gaussian],
+            xs_old: NDArray,
+            us_old: NDArray,
             ks: NDArray,
             Ks: NDArray,
             v0: NDArray,
-            Vs: NDArray,
-            Q_uu_invs: NDArray,
-            F_xs: NDArray,
-            F_us: NDArray,
-            C_xxs: NDArray,
-            C_uus: NDArray
-        ) -> Tuple[List[Gaussian], List[Gaussian], float]:
+            V0: NDArray
+        ) -> Tuple[NDArray, NDArray, float]:
         """Compute the new optimal state trajectory and control inputs.
         
         Unless otherwise specified by `self.ls_iters`, this performs a
@@ -320,20 +333,10 @@ class iLQR():
             The linear feedback vectors for the control inputs
         - `Ks`:
             The linear feedback gains for the control inputs
-        - `v1`:
+        - `v0`:
             The optimal cost-to-go vector at timestep 0
-        - `Vs`:
-            The optimal cost-to-go Hessians
-        - `Q_uu_invs`:
-            The inverses of the cost-to-go input Hessian
-        - `F_xs`:
-            The dynamic's state Jacobians
-        - `F_us`:
-            The dynamic's input Jacobians
-        - `C_xxs`:
-            The cost function's state Hessians 
-        - `C_uus`:
-            The cost function's input Hessians
+        - `V0`:
+            The optimal cost-to-go Hessian at timestep 0
         
         Returns
         - `xs`:
@@ -348,20 +351,14 @@ class iLQR():
 
         trajectory_updated = False
         if self.ls_iters == None:
-            xs, us = self._compute_trajectory(1.0, xs_old, us_old, ks, Ks, v0,
-                                              Vs, Q_uu_invs, F_xs, F_us, C_xxs,
-                                              C_uus)
-
+            xs, us = self._compute_means(1, xs_old, us_old, ks, Ks, v0, V0)
             return xs, us, self._cost(xs, us)
 
         a = 1.0
-        for _ in range(self.ls_iters + 1):
-            xs, us = self._compute_trajectory(a, xs_old, us_old, ks, Ks, v0, Vs,
-                                              Q_uu_invs, F_xs, F_us, C_xxs,
-                                              C_uus)
-
-            # Check linesearch convergence
+        for _ in range(self.ls_iters):
+            xs, us = self._compute_means(a, xs_old, us_old, ks, Ks, v0, V0)
             cost = self._cost(xs, us)
+
             if cost <= cost_old:
                 trajectory_updated = True
                 break
@@ -403,7 +400,8 @@ class iLQR():
                 The MAP control inputs
         """
 
-        xs, us = self._rollout(us_init)
+        xs = self._rollout(us_init)
+        us = np.array(us_init)
         cost = self._cost(xs, us)
         if print_iters:
             print(f"iLQR 0/{num_iters}: Cost = {cost}")
@@ -418,22 +416,20 @@ class iLQR():
 
         for i in range(num_iters):
             for t in range(self.T):
-                F_xs[t] = self.dynamics.df_dx(xs[t].mean, us[t].mean, t)
-                F_us[t] = self.dynamics.df_du(xs[t].mean, us[t].mean, t)
+                F_xs[t] = self.dynamics.df_dx(xs[t], us[t], t)
+                F_us[t] = self.dynamics.df_du(xs[t], us[t], t)
 
-                c_xs[t] = -self.meas_model.dll(xs[t].mean, self.ys[t], t)
+                c_xs[t] = -self.meas_model.dll(xs[t], self.ys[t], t)
                 C_xxs[t] = diag_regularise(
-                    -self.meas_model.d2ll(xs[t].mean, self.ys[t], t))
-                c_us[t] = -self.input_prior.dll(us[t].mean, t)
-                C_uus[t] = diag_regularise(
-                    -self.input_prior.d2ll(us[t].mean, t))
+                    -self.meas_model.d2ll(xs[t], self.ys[t], t))
+                c_us[t] = -self.input_prior.dll(us[t], t)
+                C_uus[t] = diag_regularise(-self.input_prior.d2ll(us[t], t))
 
             ks, Ks, v0, Vs, Q_uu_invs = self._backward_pass(F_xs, F_us, c_xs,
                                                             c_us, C_xxs, C_uus)
 
             xs_new, us_new, new_cost = self._forward_pass(xs, us, ks, Ks, v0,
-                                                          Vs, Q_uu_invs, F_xs,
-                                                          F_us, C_xxs, C_uus)
+                                                          Vs[0])
 
             if print_iters:
                 print(f"iLQR {i+1}/{num_iters}: Cost = {new_cost}")
@@ -445,6 +441,9 @@ class iLQR():
             us = us_new
             cost = new_cost
 
+        xs, us = self._compute_covs(xs, us, Ks, Vs, Q_uu_invs, F_xs, F_us,
+                                    C_xxs, C_uus)
+        
         return xs, us
 
 
@@ -480,7 +479,7 @@ class LQR(iLQR):
                         and isinstance(meas_model, LinearGaussianMeasurement)
                         and isinstance(input_prior, GaussianPrior))
         if not linear_model:
-            print("Warning: LQR can only infer over linear generative models correctly.")
+            print("Warning: LQR can only infer over linear Gaussian generative models correctly.")
 
         super().__init__(dynamics, meas_model, input_prior, ys, x0_prior,
                          ls_iters=None)
