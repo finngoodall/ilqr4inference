8583098ad692e4e98e41283fd9e5cee321166953
finngoodall <finn.goodall@gmail.com>
2023-04-03 22:36:08 +0100

    Gave input priors non-zero means

--- lib/models/inputs.py
+++ lib/models/inputs.py
@@ -8,21 +8,23 @@ from lib.base import AGInputPrior, InputPrior
 
 
 class GaussianPrior(InputPrior):
-    """Class for a zero-mean Gaussian prior over the control inputs to a
-    generative model.
+    """Class for a Gaussian prior over the control inputs to a generative model.
     """
 
-    def __init__(self, Nu: int, cov: NDArray):
+    def __init__(self, Nu: int, mean:NDArray, cov: NDArray):
         """Construct the Gaussian prior.
         
         Parameters
         - `Nu`:
             Number of control input dimensions
+        - `mean`:
+            Mean of the Gaussian distribution
         - `cov`:
             Covariance of the Gaussian distribution
         """
 
         self.Nu = Nu
+        self.mean = mean
         self.cov = cov
         # Store the precision matrix to use in calculations
         self._P = np.linalg.inv(self.cov)
@@ -30,13 +32,14 @@ class GaussianPrior(InputPrior):
     def sample(self, t: int) -> NDArray:
         """Sample from the control input prior at timestep `t`."""
 
-        return np.random.multivariate_normal(np.zeros(self.Nu), self.cov)
+        return np.random.multivariate_normal(self.mean, self.cov)
     
     def ll(self, u: NDArray, t: int) -> float:
-        return -0.5 * u.T @ self._P @ u
+        z = u - self.mean
+        return -0.5 * z.T @ self._P @ z
 
     def dll(self, u: NDArray, t: int) -> NDArray:
-        return -self._P @ u
+        return -self._P @ (u - self.mean)
     
     def d2ll(self, u: NDArray, t: int) -> NDArray:
         return -self._P
@@ -44,11 +47,11 @@ class GaussianPrior(InputPrior):
 
 
 class StudentPrior(AGInputPrior):
-    """Class for a zero-mean Student t-distribution prior over the control
-    inputs in a generative model.
+    """Class for a Student t-distribution prior over the control inputs in a
+    generative model.
     """
 
-    def __init__(self, Nu: int, nu: float, S: NDArray):
+    def __init__(self, Nu: int, nu: float, mean: NDArray, S: NDArray):
         """Construct the Gaussian prior.
         
         Parameters
@@ -56,18 +59,22 @@ class StudentPrior(AGInputPrior):
             Number of control input dimensions
         - `dof`:
             Degrees of freedeom of the distribution
+        - `mean`:
+            Mean of the distribution
         - `S`:
             Shape vector of the distribution
         """
 
         self.Nu = Nu
+        self.mean = mean
         self.nu = nu
         self.S = S
 
     def sample(self, t: int) -> NDArray:
         """Sample from the control input prior at timestep `t`."""
         
-        return student.rvs(df=self.nu, scale=self.S)
+        return student.rvs(df=self.nu, loc=self.mean, scale=self.S)
     
     def ll(self, u: NDArray, t: int) -> float:
-        return -np.log(1 + (u.T/self.S @ u) / self.nu)
+        z = u - self.mean
+        return -np.log(1 + (z.T/self.S @ z) / self.nu)
