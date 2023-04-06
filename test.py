32709d89399fe9c074016e4329489bfd421c4e9d
finngoodall <finn.goodall@gmail.com>
2023-04-06 23:33:46 +0100

    Updated Kalman test to match the report

--- test.py
+++ test.py
@@ -1,64 +1,66 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import autograd.numpy as np
 
 from lib.functions import sample_trajectory
-from lib.lqr import iLQR
+from lib.kalman import KalmanSmoother
+from lib.lqr import LQR, iLQR
 from lib.models.dynamics import LinearDynamics
 from lib.models.priors import GaussianPrior
-from lib.models.measurements import GaussianMeasurement
+from lib.models.measurements import LinearGaussianMeasurement
 from lib.plotters import Plotter
 
 
 
 # General parameters
-num_steps = 100
-Nx = 2
-Nu = 2
-Ny = 2
+num_steps = 50
+Nx = 1
+Nu = 1
+Ny = 1
 
 
 
-# Create the generative model
-rho = 0.8
-theta = 0.2
-A = rho*np.array([[np.cos(theta), -np.sin(theta)],
-                  [np.sin(theta), np.cos(theta)]])
-B = np.eye(Nu)
-u_cov = 0.1*np.eye(2)
-y_cov = 0.5*np.eye(2)
+# Build generative model
+A = np.array([[0.9]])
+B = np.array([[0.5]])
+C = np.array([[1]])
+u_cov = np.eye(Nu)
+y_cov = np.array([[0.1]])
 
 dynamics = LinearDynamics(Nx, Nu, A, B)
-input_prior = GaussianPrior(Nu, u_cov)
-meas_model = GaussianMeasurement(Ny, y_cov)
+meas_model = LinearGaussianMeasurement(Ny, y_cov, C)
+input_prior = GaussianPrior(Nu, np.zeros(Nu), u_cov)
+x1_prior = GaussianPrior(Nx, np.zeros(Nx), np.eye(Nx))
 
 
 
-# Sample a trajcetory generative model
+# Generate the true states and measurements
 true_xs, true_us, ys = sample_trajectory(
     dynamics,
     meas_model,
     input_prior,
-    num_steps
+    num_steps,
+    x1_prior.sample(0)
 )
 
 
 
-# Initialise and run new iLQR
-ilqr = iLQR(
-    dynamics=dynamics,
-    meas_model=meas_model,
-    input_prior=input_prior,
-    ys=ys
-)
-us_init = [np.zeros(2) for _ in range(100)]
-ilqr_new_xs, ilqr_new_us = ilqr(us_init, print_iters=True)
+# Run Kalman smoother, LQR and iLQR
+kalman_smoother = KalmanSmoother(A, B, C, u_cov, y_cov)
+kalman_xs, kalman_us = kalman_smoother(ys, x1_prior)
+
+lqr = LQR(dynamics, meas_model, input_prior, ys, x1_prior)
+lqr_xs, lqr_us = lqr()
+
+ilqr = iLQR(dynamics, meas_model, input_prior, ys, x1_prior)
+us_init = [input_prior.sample(t) for t in range(num_steps)]
+ilqr_xs, ilqr_us = ilqr(us_init, print_iters=True)
 
 
 
-# Show results
+# Plot results
 plotter = Plotter(Nx, Nu, Ny)
-plotter.plot_states(true_xs=true_xs, ilqr_xs=ilqr_new_xs)
-plotter.plot_inputs(true_us=true_us, ilqr_us=ilqr_new_us)
-plotter.plot_measurements(model=meas_model, true_ys=ys, ilqr_xs=ilqr_new_xs)
+plotter.plot_states(true_xs, kalman_xs, lqr_xs, ilqr_xs)
+plotter.plot_inputs(true_us, kalman_us, lqr_us, ilqr_us)
+plotter.plot_measurements(meas_model, ys, kalman_xs, lqr_xs, ilqr_xs)
 
 plt.show()
