c65558f22f2a447fa0e626f5675da7bf7ba33462
finngoodall <finn.goodall@gmail.com>
2023-06-08 17:14:18 +0100

    changed variable name to reflect uses for states or inputs

--- lib/models/priors.py
+++ lib/models/priors.py
@@ -2,6 +2,7 @@ from numpy.typing import NDArray
 
 import autograd.numpy as np
 from scipy.stats import t as student
+from scipy.stats import hypsecant
 
 from lib.base import AGPrior, Prior
 
@@ -10,19 +11,19 @@ from lib.base import AGPrior, Prior
 class GaussianPrior(Prior):
     """Class for a Gaussian prior over the inital state or control inputs."""
 
-    def __init__(self, Nu: int, mean: NDArray, cov: NDArray):
+    def __init__(self, Ndims: int, mean: NDArray, cov: NDArray):
         """Construct the Gaussian prior.
         
         Parameters
-        - `Nu`:
-            Number of control input dimensions
+        - `Ndims`:
+            Number of state or control input dimensions
         - `mean`:
             Mean of the Gaussian distribution
         - `cov`:
             Covariance of the Gaussian distribution
         """
 
-        self.Nu = Nu
+        self.Ndims = Ndims
         self.mean = mean
         self.cov = cov
         # Store the precision matrix to use in calculations
@@ -46,12 +47,12 @@ class GaussianPrior(Prior):
 class StudentPrior(AGPrior):
     """Class for a Student prior over the initial state or control inputs."""
 
-    def __init__(self, Nu: int, nu: float, mean: NDArray, S: NDArray):
+    def __init__(self, Ndims: int, dof: float, mean: NDArray, S: NDArray):
         """Construct the Gaussian prior.
         
         Parameters
-        - `Nu`:
-            Number of control input dimensions
+        - `Ndims`:
+            Number of state or control input dimensions
         - `dof`:
             Degrees of freedeom of the distribution
         - `mean`:
@@ -60,19 +61,62 @@ class StudentPrior(AGPrior):
             Shape vector of the distribution
         """
 
-        self.Nu = Nu
+        self.Ndims = Ndims
         self.mean = mean
-        self.nu = nu
+        self.dof = dof
         self.S = S
 
     def sample(self, t: int) -> NDArray:
-        rv = student.rvs(df=self.nu, loc=self.mean, scale=self.S)
+        rv = student.rvs(df=self.dof, loc=self.mean, scale=self.S)
         
-        if self.Nu == 1:
+        if self.Ndims == 1:
             return np.array([rv])
             
         return rv
 
     def ll(self, z: NDArray, t: int) -> float:
         v = z - self.mean
-        return -np.log(1 + (v.T/self.S @ v) / self.nu)
+        return -0.5*(1 + self.dof)*np.log(1 + (v.T/self.S @ v) / self.dof)
+
+
+
+class HypSecantPrior(Prior):
+    """Class for a hyperbolic secant prior over the intial state or control
+    inputs.
+    """
+
+    def __init__(self, Ndims: int, mean: NDArray, scale: NDArray):
+        """Construct the prior.
+        
+        Parameters
+        - `Ndims`:
+            Number of state or control input dimensions
+        - `mean`:
+            Mean of the distribution
+        - `scale`:
+            Shape vector of the distribution
+        """
+        
+        self.Ndims = Ndims
+        self.mean = mean
+        self.scale = scale
+
+    def sample(self, t: int) -> NDArray:
+        rv = hypsecant.rvs(loc=self.mean, scale=self.scale)
+        if self.Ndims == 1:
+            return np.array([rv])
+            
+        return rv
+    
+    def ll(self, z: NDArray, t: int) -> float:
+        v = (z - self.mean)/self.scale
+        return np.sum(-np.log(np.cosh(v)))
+    
+    def dll(self, z: NDArray, t: int) -> float:
+        v = (z - self.mean)/self.scale
+        return (np.exp(v) - np.exp(-v))/(np.exp(v) + np.exp(-v))
+    
+    def d2ll(self, z: NDArray, t: int) -> NDArray:
+        v = (z - self.mean)/self.scale
+        M = np.diag((np.exp(v) - np.exp(-v))/(np.exp(v) + np.exp(-v)))
+        return np.eye(self.Ndims) - M**2
