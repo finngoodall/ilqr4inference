fb57a0e8889b817e7fd8e4e32d414cede661d9e9
finngoodall <finn.goodall@gmail.com>
2023-06-08 15:41:24 +0100

    added van der pol dynamics

--- lib/models/dynamics.py
+++ lib/models/dynamics.py
@@ -110,3 +110,29 @@ class GSMDynamicsKnownContrast(Dynamics):
 
     def df_du(self, x: NDArray, u: NDArray, t: int) -> NDArray:
         return self._Umat
+
+
+
+class VanDerPolDynamics(Dynamics):
+    """Class for 2D dynamics following a discretised Van der Pol oscillator."""
+
+    def __init__(self, mu: float, dt: float = 0.1) -> None:
+        self.mu = mu
+        self.dt = dt
+        self.Nx = 2
+        self.Nu = 2
+
+    def _A(self, x):
+        return np.array([[0, 1], [-1 - self.mu*x[0]*x[1], self.mu]])
+
+    def _B(self):
+        return np.eye(self.Nu)
+
+    def f(self, x, u, t):
+        return (np.eye(self.Nx) + self.dt*self._A(x))@x + self._B()@u
+
+    def df_dx(self, x, u, t):
+        return np.eye(self.Nx) + self.dt*self._A(x)
+    
+    def df_du(self, x, u, t):
+        return self._B()
